9e85c8e511b195ea970f297c897d573e6b288e14
Jacek <steiner@10g.pl>
2023-01-24 12:25:54 +0100

    project start

---
+++ src/main/java/pl/jacekrg/AlpineGuesthouse/AlpineGuesthouseApplication.java
@@ -0,0 +1,15 @@
+package pl.jacekrg.AlpineGuesthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpineGuesthouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpineGuesthouseApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/pl/jacekrg/AlpineGuesthouse/AlpineGuesthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacekrg.AlpineGuesthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpineGuesthouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
